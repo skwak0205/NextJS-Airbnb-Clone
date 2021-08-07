4eade4dcb00fb8f37372fe39a7989a34cff38acc
Soojung Kwak <gsj2464@gmail.com>
2021-08-07 21:14:21 +0900

    header component - left section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div className=''>
       <Head>
         <title>NextJS - Airbnb</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <Header />
+
+      {/* Banner */}
     </div>
   );
 }
