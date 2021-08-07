db1e664a45730bc3a5c795fc6d15b0e25c4f65ce
Soojung Kwak <gsj2464@gmail.com>
2021-08-07 21:28:42 +0900

    header component - middle section

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/solid';
 
 const Header = () => {
   return (
@@ -12,7 +13,14 @@ const Header = () => {
         />
       </div>
 
-      <div></div>
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <input
+          className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
+          type='text'
+          placeholder='Start your search'
+        />
+        <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
+      </div>
 
       <div></div>
     </header>
