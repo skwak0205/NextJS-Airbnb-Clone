530c6da82b95a28cd563a0fa2745d4233869107a
Soojung Kwak <gsj2464@gmail.com>
2021-08-08 17:00:15 +0900

    static side rendering 추가

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className=''>
       <Head>
@@ -13,6 +13,29 @@ export default function Home() {
       <Header />
 
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {exploreData?.map((item) => (
+            
+          ))}
+        </section>
+      </main>
     </div>
   );
+}
+
+// Static Side Rendering
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
