329da39300d7d238d3afe998c952603f4c597f36
Soojung Kwak <gsj2464@gmail.com>
2021-08-09 16:11:32 +0900

    useState 추가

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import {
   SearchIcon,
@@ -8,6 +9,8 @@ import {
 } from '@heroicons/react/solid';
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState('');
+
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       <div className='relative flex items-center h-10 cursor-pointer my-auto'>
@@ -21,6 +24,8 @@ const Header = () => {
 
       <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
           type='text'
           placeholder='Start your search'
