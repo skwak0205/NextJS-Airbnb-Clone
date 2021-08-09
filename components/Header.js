606bc180130dbced22bdf8d0127350d3521c7a60
Soojung Kwak <gsj2464@gmail.com>
2021-08-09 17:14:14 +0900

    date picker 추가

--- components/Header.js
+++ components/Header.js
@@ -7,9 +7,26 @@ import {
   UserCircleIcon,
   UsersIcon,
 } from '@heroicons/react/solid';
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 const Header = () => {
   const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: 'selection',
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
 
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
@@ -42,6 +59,42 @@ const Header = () => {
           <UserCircleIcon className='h-6' />
         </div>
       </div>
+
+      {searchInput && (
+        <div className='flex flex-col col-span-3 mx-auto'>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+
+          <div className='flex items-center border-b mb-4'>
+            <h2 className='text-2xl flex-grow font-semibold'>
+              Number of Guests
+            </h2>
+
+            <UsersIcon className='h-5' />
+            <input
+              type='number'
+              value={noOfGuests}
+              className='w-12 pl-2 text-lg outline-none text-red-400'
+              min={1}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+            />
+          </div>
+
+          <div className='flex'>
+            <button
+              className='flex-grow text-gray-500'
+              onClick={() => setSearchInput('')}
+            >
+              Cancel
+            </button>
+            <button className='flex-grow text-red-400'>Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
