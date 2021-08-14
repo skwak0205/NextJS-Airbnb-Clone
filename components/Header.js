cd0b7b2a81177c67f19757d18f1b2e2ed74ade62
Soojung Kwak <gsj2464@gmail.com>
2021-08-14 20:54:50 +0900

    search component 추가

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useRouter } from 'next/dist/client/router';
 import Image from 'next/image';
 import {
   SearchIcon,
@@ -11,11 +12,13 @@ import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 
-const Header = () => {
+const Header = ({ placeholder }) => {
   const [searchInput, setSearchInput] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const router = useRouter();
 
   const selectionRange = {
     startDate,
@@ -28,9 +31,24 @@ const Header = () => {
     setEndDate(ranges.selection.endDate);
   };
 
+  const search = () => {
+    router.push({
+      pathname: '/search',
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
+
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
-      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+      <div
+        onClick={() => router.push('/')}
+        className='relative flex items-center h-10 cursor-pointer my-auto'
+      >
         <Image
           src='https://links.papareact.com/qd3'
           layout='fill'
@@ -45,7 +63,7 @@ const Header = () => {
           onChange={(e) => setSearchInput(e.target.value)}
           className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
           type='text'
-          placeholder='Start your search'
+          placeholder={placeholder || 'Start your search'}
         />
         <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
       </div>
@@ -91,7 +109,9 @@ const Header = () => {
             >
               Cancel
             </button>
-            <button className='flex-grow text-red-400'>Search</button>
+            <button className='flex-grow text-red-400' onClick={search}>
+              Search
+            </button>
           </div>
         </div>
       )}
---
+++ pages/search.js
@@ -0,0 +1,45 @@
+import { useRouter } from 'next/dist/client/router';
+import { format } from 'date-fns';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Search = () => {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+  const formattedStartDate = format(new Date(startDate), 'dd MMM yy');
+  const formattedEndDate = format(new Date(endDate), 'dd MMM yy');
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
+  return (
+    <div className='h-screen'>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests}`} />
+
+      <main className='flex'>
+        <section className='flex-grow pt-14 px-6'>
+          <p className='text-xs'>
+            300+ Stays - {range} - for {noOfGuests} guests
+          </p>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>
+            Stays in {location}
+          </h1>
+
+          <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+            <p className='button'>Cancellation Flexibility</p>
+
+            <p className='button'>Type of Place</p>
+
+            <p className='button'>Price</p>
+
+            <p className='button'>Rooms and Beds</p>
+
+            <p className='button'>More filters</p>
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Search;
